65b10a762f3ca11e5066e2c740e264b105144dca
heart14 <lwf14@qq.com>
2022-11-27 14:42:41 +0800

    optimize redis cache name ::

--- common/src/main/java/xyz/sadli/redis/RedisConfig.java
+++ common/src/main/java/xyz/sadli/redis/RedisConfig.java
@@ -7,7 +7,6 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
@@ -46,12 +45,17 @@ public class RedisConfig {
 //    }
 
 
+    /**
+     * 修改redis缓存管理器中key和value的序列化方式，若不修改，则使用的是默认的jdk序列化，存储到redis里面查看是乱码
+     * @param factory
+     * @return
+     */
     @Bean
     public RedisCacheManager redisCacheManager(LettuceConnectionFactory factory) {
-        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
-        System.out.println("init redis cache config");
         RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .computePrefixWith(name -> name + ":")//redis2.x版本中，保存缓存时默认会在cacheNames后面加上双冒号，使用该配置改为单冒号
+                .disableCachingNullValues()//禁止缓存null值
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
         return RedisCacheManager.builder(factory).cacheDefaults(configuration).build();
     }
