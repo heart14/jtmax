8d8e98b3aa838ff413736d68868ac108c92ffa6a
heart14 <lwf14@qq.com>
2022-12-02 16:29:22 +0800

    api param class

---
+++ bean/src/main/java/xyz/sadli/query/sys/LoginQuery.java
@@ -0,0 +1,49 @@
+package xyz.sadli.query.sys;
+
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * About:
+ * Other:
+ * Created: lwf14 on 2022/12/2 15:51.
+ * Editored:
+ */
+
+public class LoginQuery {
+
+    @NotEmpty
+    private String phoneNumber;
+
+    @NotEmpty
+    private String password;
+
+
+    public LoginQuery() {
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginQuery{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
