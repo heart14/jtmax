f9ac54184390b6bfa8ca19c2898887726f9c07dd
heart14 <lwf14@qq.com>
2022-12-02 17:30:31 +0800

    get player info api

--- dashboard-service/src/main/java/xyz/sadli/controller/JtPlayerController.java
+++ dashboard-service/src/main/java/xyz/sadli/controller/JtPlayerController.java
@@ -1,17 +1,11 @@
 package xyz.sadli.controller;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.sadli.service.JtPlayerService;
-import xyz.sadli.util.SysResponseUtils;
-import xyz.sadli.vo.JtPlayerVO;
-import xyz.sadli.vo.SysResponse;
 
 /**
  * About:
@@ -32,16 +26,4 @@ public class JtPlayerController {
         this.jtPlayerService = jtPlayerService;
     }
 
-    /**
-     * 获取用户信息
-     *
-     * @param uid
-     * @return
-     */
-    @ApiOperation("获取用户信息")
-    @RequestMapping(value = "/getUserInfo/{uid}", method = RequestMethod.GET)
-    public SysResponse getUserInfo(@PathVariable("uid") String uid) {
-        JtPlayerVO player = jtPlayerService.queryPlayerByUid(uid);
-        return SysResponseUtils.success(player);
-    }
 }
