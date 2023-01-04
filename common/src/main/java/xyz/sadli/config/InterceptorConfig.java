49e7417ebfed8490d27cf28661e8ef6d3e2fddb9
heart14 <lwf14@qq.com>
2023-01-04 16:30:56 +0800

    (*)跨域请求处理

--- common/src/main/java/xyz/sadli/config/InterceptorConfig.java
+++ common/src/main/java/xyz/sadli/config/InterceptorConfig.java
@@ -20,23 +20,4 @@ public class InterceptorConfig implements WebMvcConfigurer {
         //注册自定义过滤器
         registry.addInterceptor(new RequestInterceptor());
     }
-
-    @Override
-    //重写父类提供的跨域请求处理的接口
-    public void addCorsMappings(CorsRegistry registry) {
-        //添加映射路径
-        registry.addMapping("/**")
-                //放行哪些原始域
-                .allowedOrigins("*")
-                //是否发送Cookie信息
-                .allowCredentials(true)
-                //放行哪些原始域(请求方式)
-                .allowedMethods("OPTIONS","GET", "POST", "PUT", "DELETE", "HEAD", "FETCH")
-                //放行哪些原始域(头部信息)
-                .allowedHeaders("*")
-                //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
-                .exposedHeaders("Authorization");
-
-        WebMvcConfigurer.super.addCorsMappings(registry);
-    }
 }
