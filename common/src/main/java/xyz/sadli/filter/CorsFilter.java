9aa12263fb42d5114423c811fd10495dd1fc700e
heart14 <lwf14@qq.com>
2023-01-05 17:58:27 +0800

    (*)优化请求过滤器 (+)配置文件新增字符编码

--- common/src/main/java/xyz/sadli/filter/CorsFilter.java
+++ common/src/main/java/xyz/sadli/filter/CorsFilter.java
@@ -18,14 +18,13 @@ import java.io.IOException;
  * Created: wfli on 2023/1/4 17:08.
  * Editored:
  */
-@Component
+@Component // 为什么用这个@Component就可以，难道不要用@WebFilter吗
 public class CorsFilter implements Filter {
 
     public static final Logger log = LoggerFactory.getLogger(CorsFilter.class);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        log.info("CorsFilter init");
         Filter.super.init(filterConfig);
     }
 
@@ -35,21 +34,21 @@ public class CorsFilter implements Filter {
         if (MDC.get(Constants.FIELD_MDC_TRACE_ID) == null) {
             MDC.put(Constants.FIELD_MDC_TRACE_ID, StringUtils.UuidLowerCase());
         }
-
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        log.info("CorsFilter doFilter, request METHOD = {}, request URL = {}",request.getMethod(), request.getRequestURL());
+        log.info("Request filter, METHOD = {}, URL = {}", request.getMethod(), request.getRequestURL());
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Credentials","true");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE,OPTIONS,FETCH,HEAD");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header, Authorization");
-        filterChain.doFilter(request,response);
+        filterChain.doFilter(request, response);
+        //请求完成后清除traceId
+        MDC.clear();
     }
 
     @Override
     public void destroy() {
-        log.info("CorsFilter destroy");
         Filter.super.destroy();
     }
 }
