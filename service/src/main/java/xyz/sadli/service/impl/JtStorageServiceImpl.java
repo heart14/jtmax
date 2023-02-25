f7651eee7521b6ddda62b3c7dc6281929db02d0b
heart14 <lwf14@qq.com>
2023-02-26 00:18:15 +0800

    (*)文件上传支持上传视频 (*)优化文件存储路径配置

--- service/src/main/java/xyz/sadli/service/impl/JtStorageServiceImpl.java
+++ service/src/main/java/xyz/sadli/service/impl/JtStorageServiceImpl.java
@@ -46,42 +46,40 @@ public class JtStorageServiceImpl implements JtStorageService {
     @Override
     public JtStorage upload(MultipartFile file, String creatorUid) {
         Assert.notNull(file, ErrCodeEnums.PARAMS_EXCEPTION.getMsg());
+        typeCheck(file.getContentType());
         Calendar calendar = Calendar.getInstance();
         String root = calendar.get(Calendar.YEAR) + Constants.URL_SEPARATOR + (calendar.get(Calendar.MONTH) + 1) + Constants.URL_SEPARATOR + calendar.get(Calendar.DAY_OF_MONTH);
-        String targetDirPath = SysProperties.BUCKET_PATH + Constants.URL_SEPARATOR + root;
+        String targetDirPath = SysProperties.BUCKET_PATH + file.getContentType().split("/")[0] + Constants.URL_SEPARATOR + root;
         File targetDir = new File(targetDirPath);
         if (!targetDir.exists()) {
             targetDir.mkdirs();
         }
-        if (MediaType.IMAGE_JPEG_VALUE.equalsIgnoreCase(file.getContentType()) || MediaType.IMAGE_PNG_VALUE.equalsIgnoreCase(file.getContentType()) || MediaType.IMAGE_GIF_VALUE.equalsIgnoreCase(file.getContentType())) {
-            String uuid = StringUtils.UuidLowerCase();
-            String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")).toLowerCase(Locale.ROOT);
-            String filePath = targetDirPath + Constants.URL_SEPARATOR + uuid + suffix;
-            try {
-                // 存储
-                file.transferTo(new File(filePath));
-            } catch (IOException e) {
-                throw new SysException(ErrCodeEnums.FAILED_UPLOAD_EXCEPTION.getCode(), ErrCodeEnums.FAILED_UPLOAD_EXCEPTION.getMsg());
-            }
-            String url = SysProperties.BUCKET_URL + Constants.URL_SEPARATOR + root + Constants.URL_SEPARATOR + uuid + suffix;
+        String uuid = StringUtils.UuidLowerCase();
+        String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")).toLowerCase(Locale.ROOT);
+        String filePath = targetDirPath + Constants.URL_SEPARATOR + uuid + suffix;
+        try {
             // 存储
-            JtStorage jtStorage = new JtStorage();
-            jtStorage.setId(IdWorker.nextIdStr());
-            jtStorage.setOriginName(file.getOriginalFilename());
-            jtStorage.setStorageName(uuid + suffix);
-            jtStorage.setSize(String.valueOf(file.getSize()));
-            jtStorage.setStoragePath(filePath);
-            jtStorage.setNetworkUrl(url);
-            jtStorage.setMediaType(file.getContentType());
-            jtStorage.setResourceType(Constants.RESOURCE_TYPE_LIBRARY);
-            jtStorage.setCreator(creatorUid);
-            jtStorage.setStatus(Constants.STATUS_VALID);
-            jtStorage.setCreateTime(new Date());
-            storageMapper.insertSelective(jtStorage);
-            return jtStorage;
-        } else {
-            throw new SysException(ErrCodeEnums.MEDIA_TYPE_EXCEPTION.getCode(), ErrCodeEnums.MEDIA_TYPE_EXCEPTION.getMsg());
+            file.transferTo(new File(filePath));
+        } catch (IOException e) {
+            throw new SysException(ErrCodeEnums.FAILED_UPLOAD_EXCEPTION.getCode(), ErrCodeEnums.FAILED_UPLOAD_EXCEPTION.getMsg());
         }
+        String url = SysProperties.BUCKET_URL + file.getContentType().split("/")[0] + Constants.URL_SEPARATOR + root + Constants.URL_SEPARATOR + uuid + suffix;
+
+        // 存储
+        JtStorage jtStorage = new JtStorage();
+        jtStorage.setId(IdWorker.nextIdStr());
+        jtStorage.setOriginName(file.getOriginalFilename());
+        jtStorage.setStorageName(uuid + suffix);
+        jtStorage.setSize(String.valueOf(file.getSize()));
+        jtStorage.setStoragePath(filePath);
+        jtStorage.setNetworkUrl(url);
+        jtStorage.setMediaType(file.getContentType());
+        jtStorage.setResourceType(Constants.RESOURCE_TYPE_LIBRARY);
+        jtStorage.setCreator(creatorUid);
+        jtStorage.setStatus(Constants.STATUS_VALID);
+        jtStorage.setCreateTime(new Date());
+        storageMapper.insertSelective(jtStorage);
+        return jtStorage;
     }
 
     @Override
@@ -99,5 +97,21 @@ public class JtStorageServiceImpl implements JtStorageService {
         PageHelper.startPage(query.getPage(), query.getLimit());
         List<JtStorage> storageList = storageMapper.selectStorageListByQuery(query);
         return new PageInfo<>(storageList);
+    }
+
+    /**
+     * 检查文件类型是否合法，后期可以将这里检查的类型由枚举类型统一提供
+     *
+     * @param contentType
+     * @return
+     */
+    private void typeCheck(String contentType) {
+        if ("video/mp4".equals(contentType)) {
+        } else if (MediaType.IMAGE_PNG_VALUE.equals(contentType)) {
+        } else if (MediaType.IMAGE_JPEG_VALUE.equals(contentType)) {
+        } else if (MediaType.IMAGE_GIF_VALUE.equals(contentType)) {
+        } else {
+            throw new SysException(ErrCodeEnums.MEDIA_TYPE_EXCEPTION.getCode(), ErrCodeEnums.MEDIA_TYPE_EXCEPTION.getMsg());
+        }
     }
 }
