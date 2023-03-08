4d35528331351ef226eeb9b68e8d414b4a4257c8
heart14 <lwf14@qq.com>
2023-03-08 22:13:13 +0800

    (+)新增PageQuery测试类

--- dashboard-service/src/test/java/xyz/sadli/test/PageTest.java
+++ dashboard-service/src/test/java/xyz/sadli/test/PageTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import xyz.sadli.query.photo.BannerPageQuery;
+import xyz.sadli.query.banner.BannerPageQuery;
 import xyz.sadli.query.photo.PhotoPageQuery;
 import xyz.sadli.service.JtStorageService;
 
