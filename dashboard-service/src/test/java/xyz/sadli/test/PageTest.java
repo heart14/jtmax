3c23b19894be991d51163552f3a9ba0c4214257a
heart14 <lwf14@qq.com>
2023-03-10 16:04:34 +0800

    (-)单元测试类移除对已删除方法的调用

--- dashboard-service/src/test/java/xyz/sadli/test/PageTest.java
+++ dashboard-service/src/test/java/xyz/sadli/test/PageTest.java
@@ -5,8 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import xyz.sadli.query.banner.BannerPageQuery;
-import xyz.sadli.query.photo.PhotoPageQuery;
 import xyz.sadli.service.JtStorageService;
 
 /**
@@ -23,18 +21,6 @@ public class PageTest {
     private JtStorageService storageService;
 
     @Test
-    public void test(){
-        PhotoPageQuery photoPageQuery = new PhotoPageQuery();
-        photoPageQuery.setLimit(10);
-        photoPageQuery.setPage(0);
-        System.out.println("1  "+photoPageQuery);
-
-        BannerPageQuery bannerPageQuery = new BannerPageQuery();
-        bannerPageQuery.setLimit(10);
-        bannerPageQuery.setPage(0);
-        System.out.println("2  "+bannerPageQuery);
-
-        System.out.println("3  "+storageService.queryStoragePageList(photoPageQuery));
-        System.out.println("4  "+storageService.queryStoragePageList(bannerPageQuery));
+    public void test() {
     }
 }
--- dashboard-service/src/test/java/xyz/sadli/test/StorageTest.java
+++ dashboard-service/src/test/java/xyz/sadli/test/StorageTest.java
@@ -1,13 +1,10 @@
 package xyz.sadli.test;
 
-import com.github.pagehelper.PageInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import xyz.sadli.entity.JtStorage;
-import xyz.sadli.query.storage.StorageBasePageQuery;
 import xyz.sadli.service.JtStorageService;
 
 /**
@@ -24,15 +21,6 @@ public class StorageTest {
     private JtStorageService storageService;
 
     @Test
-    public void pageList(){
-        StorageBasePageQuery photoPageQuery = new StorageBasePageQuery();
-        photoPageQuery.setPage(1);
-        photoPageQuery.setLimit(5);
-
-        photoPageQuery.setStatus(0);
-
-
-        PageInfo<JtStorage> pageInfo = storageService.queryStoragePageList(photoPageQuery);
-        System.out.println(pageInfo);
+    public void pageList() {
     }
 }
