8a5337ec81a9ceeb0ab7c5720e9035547d940c15
heart14 <lwf14@qq.com>
2023-03-10 16:05:14 +0800

    (*)优化图片列表分页查询条件query

--- bean/src/main/java/xyz/sadli/query/photo/PhotoPageQuery.java
+++ bean/src/main/java/xyz/sadli/query/photo/PhotoPageQuery.java
@@ -13,6 +13,15 @@ public class PhotoPageQuery extends StorageBasePageQuery {
     // 图库的resourceType值固定为1
     private Integer resourceType = 1;
 
+    // 图片状态
+    private Integer deleteStatus;
+
+    // 点赞数
+    private Integer likesNumber;
+
+    // 下载数
+    private Integer dlNumber;
+
     public PhotoPageQuery() {
     }
 
@@ -24,10 +33,37 @@ public class PhotoPageQuery extends StorageBasePageQuery {
         this.resourceType = resourceType;
     }
 
+    public Integer getDeleteStatus() {
+        return deleteStatus;
+    }
+
+    public void setDeleteStatus(Integer deleteStatus) {
+        this.deleteStatus = deleteStatus;
+    }
+
+    public Integer getLikesNumber() {
+        return likesNumber;
+    }
+
+    public void setLikesNumber(Integer likesNumber) {
+        this.likesNumber = likesNumber;
+    }
+
+    public Integer getDlNumber() {
+        return dlNumber;
+    }
+
+    public void setDlNumber(Integer dlNumber) {
+        this.dlNumber = dlNumber;
+    }
+
     @Override
     public String toString() {
         return "PhotoPageQuery{" +
                 "resourceType=" + resourceType +
+                ", deleteStatus=" + deleteStatus +
+                ", likesNumber=" + likesNumber +
+                ", dlNumber=" + dlNumber +
                 '}';
     }
 }
